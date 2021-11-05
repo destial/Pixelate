8fe4456f770173e10cd7839d4caa8e25394ceb59
coolfire02 <coolfire02@vortexnetwork.me>
2021-11-05 22:54:24 +0800

    Primitive perlin noise based terrain generation

--- app/src/main/java/xyz/destiall/pixelate/environment/generator/GeneratorBasic.java
+++ app/src/main/java/xyz/destiall/pixelate/environment/generator/GeneratorBasic.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import xyz.destiall.pixelate.Game;
 import xyz.destiall.pixelate.entities.Entity;
 import xyz.destiall.pixelate.environment.World;
+import xyz.destiall.pixelate.environment.noise.PerlinNoise;
 import xyz.destiall.pixelate.environment.tiles.Tile;
 import xyz.destiall.pixelate.environment.tiles.TileGround;
 import xyz.destiall.pixelate.environment.tiles.TileWood;
@@ -16,13 +17,24 @@ public class GeneratorBasic implements Generator {
         generate(0, world, tiles);
     }
 
+    private float generationScale = 0.05f;
+
     @Override
     public void generate(int seed, World world, Collection<Tile> tiles) {
         int i = 0;
+        double divisor = 1.0/Tile.SIZE;
+
+        double min = 1, max = 0;
         for (int x = -Game.WIDTH; x <= Game.WIDTH; x+=Tile.SIZE) {
             for (int y = -Game.HEIGHT; y <= Game.HEIGHT; y+=Tile.SIZE) {
                 Tile tile;
-                if (Math.random() < 0.5 && x > Game.WIDTH / 1.5) {
+
+                double noiseValue = PerlinNoise.noise(x*divisor*generationScale,y*divisor*generationScale);
+
+                if(noiseValue < min) min = noiseValue;
+                if(noiseValue > max) max = noiseValue;
+
+                if (noiseValue > 0.4) {
                     tile = new TileWood(i++, (int) x, (int) y, world);
                 } else {
                     tile = new TileGround(i++, (int) x, (int) y, world);
@@ -31,5 +43,7 @@ public class GeneratorBasic implements Generator {
                 tiles.add(tile);
             }
         }
+
+        //System.out.println("Noise minmax: " + min + " , " + max);
     }
 }
---
+++ app/src/main/java/xyz/destiall/pixelate/environment/noise/PerlinNoise.java
@@ -0,0 +1,96 @@
+package xyz.destiall.pixelate.environment.noise;
+
+import xyz.destiall.pixelate.position.Vector2;
+
+public class PerlinNoise {
+
+    //Randomised permutation array as designed by Perlin
+    private static int[] P = makePermutation();
+
+    //2D noise generation
+    public static double noise(double x, double y)
+    {
+        int X = (int)Math.floor(x) & 255;
+        int Y = (int)Math.floor(y) & 255;
+
+        double xf = x-Math.floor(x);
+        double yf = y-Math.floor(y);
+
+        Vector2 topRight = new Vector2(xf-1.0, yf-1.0);
+        Vector2 topLeft = new Vector2(xf, yf-1.0);
+        Vector2 bottomRight = new Vector2(xf-1.0, yf);
+        Vector2 bottomLeft = new Vector2(xf, yf);
+
+        //Select value from randomised array
+        int valueTopRight = P[P[X+1]+Y+1];
+        int valueTopLeft = P[P[X]+Y+1];
+        int valueBottomRight = P[P[X+1]+Y];
+        int valueBottomLeft = P[P[X]+Y];
+
+        double dotTopRight = topRight.dot(getConstantVector(valueTopRight));
+        double dotTopLeft = topLeft.dot(getConstantVector(valueTopLeft));
+        double dotBottomRight = bottomRight.dot(getConstantVector(valueBottomRight));
+        double dotBottomLeft = bottomLeft.dot(getConstantVector(valueBottomLeft));
+
+        double u = fade(xf);
+        double v = fade(yf);
+
+        return lerp(u, lerp(v, dotBottomLeft, dotTopLeft), lerp(v, dotBottomRight, dotTopRight));
+    }
+
+    //Permutation table initializer
+    private static int[] makePermutation()
+    {
+        int[] permutation = new int[512];
+        for(int i = 0; i < 256; ++i) permutation[i] = i;
+        shuffle(permutation, 0, 256);
+
+        //Add shuffled to prevent IndexOutOfBounds
+        for(int i = 256; i < 512; ++i) permutation[i] = permutation[i-256];
+
+        return permutation;
+    }
+
+    //Optimized perlin fade function
+    private static double fade(double t)
+    {
+        return ((6.0*t - 15)*t + 10.0)*t*t*t;
+    }
+
+    //Linear interpolation function
+    private static double lerp(double t, double a1, double a2)
+    {
+        return a1 + t*(a2-a1);
+    }
+
+    //Permutation table shuffler
+    private static void shuffle(int[] table, int start, int end)
+    {
+        for(int e = end-1; e > start; --e)
+        {
+            int i = (int) Math.round(Math.random() * (e-1));
+            int temp = table[e];
+
+            table[e] = table[i];
+            table[i] = temp;
+        }
+    }
+
+    private static Vector2 getConstantVector(int v)
+    {
+        //v is the value from the randomised permutation table
+        int a = v & 3;
+        switch(a)
+        {
+            case 0:
+                return new Vector2(1.0, 1.0);
+            case 1:
+                return new Vector2(-1.0,1.0);
+            case 2:
+                return new Vector2(-1.0, -1.0);
+            default:
+                return new Vector2(1.0, -1.0);
+        }
+    }
+
+}
